197f98b39840df2f9b6044efbe890d8f2eeb9a0f
galstudio <galstudio@gmail.com>
2023-04-18 15:23:10 +0300

    Initial Commit

--- system/util/Filter.php
+++ system/util/Filter.php
@@ -1,11 +1,10 @@
 <?php
-# 安全过滤类
+# Фильтр безопасности
 namespace system\util;
 
 class Filter
 {
     /**
-     * 字符串过滤
      * @param string|array $date
      * @param boolean $force
      * @return string|array
@@ -38,14 +37,13 @@ class Filter
     }
 
     /**
-     * 字符串还原
      * @param string|array $date
      * @return string|array
      */
     public function out($data)
     {
         if (is_string($data))
-        {            
+        {
             return $data = stripslashes($data);
         }
         else
@@ -66,77 +64,75 @@ class Filter
     }
 
     /**
-    * 去除XSS（跨站脚本攻击）
     * @param string $val
     * @return string
     **/
     public function RemoveXSS($val)
-    { 
+    {
        $val = preg_replace('/([\x00-\x08,\x0b-\x0c,\x0e-\x19])/', '', $val);
 
-       $search = 'abcdefghijklmnopqrstuvwxyz'; 
+       $search = 'abcdefghijklmnopqrstuvwxyz';
 
-       $search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';  
+       $search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-       $search .= '1234567890!@#$%^&*()'; 
+       $search .= '1234567890!@#$%^&*()';
 
-       $search .= '~`";:?+/={}[]-_|\'\\'; 
+       $search .= '~`";:?+/={}[]-_|\'\\';
 
        for ($i = 0; $i < strlen($search); $i++)
-       { 
-          $val = preg_replace('/(&#[xX]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val); 
+       {
+          $val = preg_replace('/(&#[xX]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val);
 
-          $val = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $val); 
-       } 
+          $val = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $val);
+       }
 
-       $ra1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'base', 'style'); 
+       $ra1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'base', 'style');
 
-       $ra2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload'); 
-       
-       $ra = array_merge($ra1, $ra2); 
-     
+       $ra2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
+
+       $ra = array_merge($ra1, $ra2);
+
        $found = true;
 
        while ($found == true)
-       { 
-          $val_before = $val; 
+       {
+          $val_before = $val;
 
           for ($i = 0; $i < sizeof($ra); $i++)
-          { 
-             $pattern = '/'; 
+          {
+             $pattern = '/';
 
              for ($j = 0; $j < strlen($ra[$i]); $j++)
-             { 
+             {
                 if ($j > 0)
-                { 
-                   $pattern .= '(';  
-                   $pattern .= '(&#[xX]0{0,8}([9ab]);)'; 
-                   $pattern .= '|';  
-                   $pattern .= '|(&#0{0,8}([9|10|13]);)'; 
-                   $pattern .= ')*'; 
-                } 
+                {
+                   $pattern .= '(';
+                   $pattern .= '(&#[xX]0{0,8}([9ab]);)';
+                   $pattern .= '|';
+                   $pattern .= '|(&#0{0,8}([9|10|13]);)';
+                   $pattern .= ')*';
+                }
 
-                $pattern .= $ra[$i][$j]; 
-             } 
+                $pattern .= $ra[$i][$j];
+             }
 
-             $pattern .= '/i';  
+             $pattern .= '/i';
 
-             $replacement = substr($ra[$i], 0, 2).'<xss>'.substr($ra[$i], 2); 
+             $replacement = substr($ra[$i], 0, 2).'<xss>'.substr($ra[$i], 2);
 
-             $val = preg_replace($pattern, $replacement, $val); 
+             $val = preg_replace($pattern, $replacement, $val);
 
              if ($val_before == $val)
              {
-                $found = false;  
-             }  
-          }  
-       }  
+                $found = false;
+             }
+          }
+       }
 
-       return $val;  
+       return $val;
     }
 
     /**
-     * 帖子过滤处理
      * @param string $date
      * @return string
      */
@@ -148,7 +144,6 @@ class Filter
     }
 
     /**
-     * 帖子输出处理
      * @param string $date
      * @return string
      */
@@ -157,7 +152,7 @@ class Filter
         $data = $this->out($data);
 
         // $data = htmlspecialchars($data);
-        
+
         return trim($data);
     }
 }
